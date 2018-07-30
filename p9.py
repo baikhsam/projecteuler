5e2b0bc04ae757e8fa89a3317fc2c11d81bbf77e
Sam Baik <40993337+baikhsam@users.noreply.github.com>
2018-07-30 10:53:25 +0800

    Update p9.py

--- p9.py
+++ p9.py
@@ -1,11 +1,12 @@
+import math
 a = 0
 b = 0
 c = 0 
 answer = 1
-for i in range(1, 200):
-  for j in range(1, 200):
-    for k in range(1, 200):
-      if k ** 2 == i ** 2 + j ** 2:
-        if k + i + j == 1000:
+for i in range(1, 1001):
+  for j in range(1, 1001):
+    for k in range(1, 1001):
+      if k + i + j == 1000:
+        if k ** 2 == (i ** 2) + j ** 2:
           answer = k * i * j
 print(answer)
